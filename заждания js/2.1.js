b2a92e2f91fb9bf5c5ed83a985802d990a696c17
Markizzzzzzzzzz <149126341+Markizzzzzzzzzz@users.noreply.github.com>
2023-12-18 15:37:59 +0900

    Add files via upload

---
+++ заждания js/2.1.js
@@ -0,0 +1,16 @@
+const a="10"
+console.log("ПЕРЕМЕННАЯ a",a,typeof a);
+const b="6"
+console.log("ПЕРЕМЕННАЯ b",b,typeof b);
+const c="0"
+console.log("ПЕРЕМЕННАЯ c",c,typeof c);
+const d="1"
+console.log("ПЕРЕМЕННАЯ d",d,typeof d);
+const e="4"
+console.log("ПЕРЕМЕННАЯ e",e,typeof e);
+const f="2"
+console.log("ПЕРЕМЕННАЯ f",f,typeof f);
+const g="9"
+console.log("ПЕРЕМЕННАЯ g",g,typeof g);
+const h="3"
+console.log("ПЕРЕМЕННАЯ h",h,typeof h);
---
+++ заждания js/2.2.js
@@ -0,0 +1,11 @@
+let age = "Хи-хи-хи";
+age = 10;
+alert(age);
+
+let bool = 6 < 2;
+bool = null;
+alert(bool);
+
+let num = 8989;
+num = "8989";
+alert(num);
